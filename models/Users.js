1997f75858a90b6ec0bdd3676e66cb60843d06ec
Gurveer Madurai <maduraigurveer@gmail.com>
2023-07-10 12:37:32 -0700

    models updated

--- models/Users.js
+++ models/Users.js
@@ -1,21 +1,39 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-  },
-  thoughts: [],
-  friends: [],
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
