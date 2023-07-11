e9456231775a915b4538aea5d290c59318538f11
Gurveer Madurai <maduraigurveer@gmail.com>
2023-07-10 22:58:55 -0700

    thoughts conroller updated

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,1 +1,36 @@
-const {Thoughts} = require('../models');
+const { Thoughts } = require("../models");
+
+module.exports = {
+  //get all thoughts
+  async getAllThoughts(req, res) {
+    try {
+      const thoughtsData = await Thoughts.find({});
+      res.status(200).json(thoughtsData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getThoughtById(req, res) {
+    try {
+      const thoughtsData = await Thoughts.findOne({
+        _id: req.params.thoughtId,
+      }).select("-__v");
+      if (!thoughtsData) {
+        return res
+          .status(404)
+          .json({ message: "No thoughts found with this id!" });
+      }
+      res.json(thoughtsData);
+    } catch (err) {
+      res.send(500).json(err);
+    }
+  },
+  async createThought(req, res) {
+    try {
+      const thoughtsData = await Thoughts.create(req.body);
+      res.json(thoughtsData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
